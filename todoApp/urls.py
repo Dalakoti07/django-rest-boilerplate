699b70dc5f8c6bce48099e53f5fde63ea26a54af
dalakoti07 <saurabhdalakoti07@gmail.com>
2020-05-05 18:47:53 +0530

    static url configured

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -21,4 +21,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('hostedApis.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
